15c866dc626807560526ede2a0be160ab734fa9e
LocTM <tranminhloc.anhk@gmail.com>
2024-03-27 09:16:14 +0700

    first-cm

---
+++ src/main/java/com/example/citymanager/CitymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.citymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitymanagerApplication.class, args);
+    }
+
+}
